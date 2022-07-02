d978915945abbadc6d3e2d8ca446533e751c8981
Timofei Shadrin <tshadrin@yandex.ru>
2022-07-02 16:06:04 +0300

    Task 23 finished

---
+++ Task23/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Clear();
+                Console.Write("Enter a number: ");
+                string? number = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(number))
+                {
+                    int digit = Convert.ToInt32(number.ToString());
+                    Console.WriteLine("Your numbers in the third power: ");
+                    for (int i = 1; i <= digit; i++)
+                    {
+                        int result = i * i * i;
+                        Console.Write($"{result} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            catch
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+    }
+}
